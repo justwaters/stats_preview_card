1ca43f46311fa5fc7710b49eedf78706504525dd
justwaters <justin@1waters.com>
2024-12-03 09:32:38 -0500

    Added theme features, toggle button, and removed theme media queries

---
+++ script.js
@@ -0,0 +1,21 @@
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+function updateThemePreference(e) {
+	if (e.matches) {
+		document.body.classList.toggle("dark");
+	} else {
+		document.body.classList.toggle("light-mode");
+	}
+}
+
+// Initial theme check
+updateThemePreference(prefersDarkScheme);
+
+// Add listener for theme change
+prefersDarkScheme.addEventListener("change", updateThemePreference);
+
+// Mode toggle button
+document.getElementById("mode-button").addEventListener("click", () => {
+	document.body.classList.toggle("light-mode");
+	document.body.classList.toggle("dark-mode");
+});

eeced43b51e77b52680613bbd2e2b0bef30dec7a
justwaters <justin@1waters.com>
2024-12-03 09:52:37 -0500

    Updated theme switcher

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+const prefersLightScheme = window.matchMedia("(prefers-color-scheme: light)");
 
 function updateThemePreference(e) {
 	if (e.matches) {
-		document.body.classList.toggle("dark-mode");
+		// If dark scheme is matched
+		document.body.classList.add("dark-mode");
+		document.body.classList.remove("light-mode");
 	} else {
-		document.body.classList.toggle("light-mode");
+		// If light scheme is matched
+		document.body.classList.add("light-mode");
+		document.body.classList.remove("dark-mode");
 	}
 }
 
 // Initial theme check
 updateThemePreference(prefersDarkScheme);
 
-// Add listener for theme change
+// Add listeners for theme change
 prefersDarkScheme.addEventListener("change", updateThemePreference);
+prefersLightScheme.addEventListener("change", updateThemePreference);
 
 // Mode toggle button
 document.getElementById("mode-button").addEventListener("click", () => {
-	document.body.classList.toggle("light-mode");
-	document.body.classList.toggle("dark-mode");
+	if (document.body.classList.contains("dark-mode")) {
+		document.body.classList.remove("dark-mode");
+		document.body.classList.add("light-mode");
+	} else {
+		document.body.classList.remove("light-mode");
+		document.body.classList.add("dark-mode");
+	}
 });
